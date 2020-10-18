809c91fca3257819d098bf78f2a87e9698a4c959
Viktor Blidh <55929893+vblidh@users.noreply.github.com>
2020-10-18 23:35:26 +0200

    Removed unnecessary files

--- build/lib/restapi/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import os
-
-
-engine = create_engine("sqlite:///formula1_database.db", echo=True,pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
